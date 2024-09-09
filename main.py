157276f20b27653d485d58307fa0378048ce20b1
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-09-09 15:51:32 +0200

    [refactor]: Moved functions to own files and updated docstrings

--- main.py
+++ main.py
@@ -1,63 +1,39 @@
+import prompts
 from products import Product
 from store import Store
-from typing import Callable
-
-ProgramChoices = list[tuple[str, Callable]]
-ProductList = list[Product]
+from dispatcher import dispatcher
 
 
-def prompt_store_menu(program_choices: ProgramChoices) -> int:
-    """
-        Prompts the user for a selection of the store menu. The store menu consists of
-        all available store functions. Keeps prompting the user until a valid input is received.
-     """
+def start(store: Store) -> None:
+    """ Prompts the desired store-program and dispatches the selection. """
+
     while True:
-        print("   Store Menu\n   ----------")
-        for idx, (label, func) in enumerate(program_choices):
-            print(f"{idx + 1}. {label}")
+        # Get validated program-selection from user.
+        choice = prompts.prompt_store_menu(dispatcher)
 
-        try:
-            choice = int(input("Please choose a number: "))
-        except ValueError:
-            print(f"Please enter your selection as a number.")
-            continue
+        # Quit the program if the last option is selected
+        # (not ideal, but quit should always be the last option in the list).
+        if choice == len(dispatcher):
+            print("Goodbye")
+            return
 
-        if not 1 <= choice <= len(program_choices):
-            print(f"Please select a number from 1 to {len(program_choices)}")
-            continue
-
-        return choice
-
-
-def start(
-        product_list: ProductList,
-        program_choices: ProgramChoices
-) -> None:
-    """ Starts the program. """
-
-    # Get validated program selection from user
-    choice = prompt_store_menu(program_choices)
-    print(choice)
+        # Dispatch selected option.
+        print("   -----")
+        dispatcher[choice - 1]["func"](store)
+        print("   -----", end="\n\n")
 
 
 def main():
+    """ Initialises the best-buy Instance and starts the program. """
     # setup initial stock of inventory
-    product_list: ProductList = [
+    product_list = [
         Product("MacBook Air M2", price=1450, quantity=100),
         Product("Bose QuietComfort Earbuds", price=250, quantity=500),
         Product("Google Pixel 7", price=500, quantity=250)
     ]
     best_buy = Store(product_list)
 
-    # program-choices
-    program_choices = [
-        ("List all products in store", lambda x: x),
-        ("Show total amount in store", lambda x: x),
-        ("Make an order", lambda x: x),
-        ("Quit", lambda x: x),
-    ]
-
-    start(product_list, program_choices)
+    start(best_buy)
 
 
 if __name__ == '__main__':
